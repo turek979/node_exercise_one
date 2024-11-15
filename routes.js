a0703e94271a19be6acad319ae8c083e39c0deb2
J.Turowski <turek.priv@gmail.com>
2024-11-15 08:44:11 +0100

    added a response to form and rerouting

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ const listOfUsers = ["user1", "user2", "user3", "user4"];
 function requestHandler(request, response) {
   const url = request.url;
   const method = request.method;
+  let tableOfUsers = "";
 
   if (url === "/") {
     response.write(`
@@ -11,17 +12,19 @@ function requestHandler(request, response) {
                     <title>Greetings</title>
                 </head>
                 <body>
-                <h1 style="text-align: center">Greetings on my exercise!</h1>
+                <h1 style="text-align: center">Greetings on my exercise!</h1></br></br>
+                <div style="text-align: center">
+                <form action="/create-user" method="POST"><input type="text" name="new_user"><button type="submit">Send</button></form>
+                </div>
                 </body>
             </html>
             `);
     return response.end();
   }
   if (url === "/users") {
-    let tableOfUsers = "";
     for (let i in listOfUsers) {
       tableOfUsers = tableOfUsers + `<li>${listOfUsers[i]}</li>`;
-    //   console.log(tableOfUsers);
+      //   console.log(tableOfUsers);
     }
     response.write(`
             <html>
@@ -36,6 +39,20 @@ function requestHandler(request, response) {
             </html>
             `);
     return response.end();
+  }
+  if ((url === "/create-user") & (method === "POST")) {
+    const dataBody = [];
+    request.on("data", (chunk) => {
+      dataBody.push(chunk);
+    });
+    return request.on("end", () => {
+      const parsedBody = Buffer.concat(dataBody).toString();
+      const newUser = parsedBody.split("=")[1];
+      listOfUsers.push(newUser);
+      response.statusCode = 302;
+      response.setHeader("Location", "/users");
+      return response.end();
+    });
   }
 }
 
